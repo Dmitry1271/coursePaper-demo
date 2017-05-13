5eead8cb4ff4d9c7a9ce185c46738f04e94c42f3
Dmitry <Dmitrij1271@gmail.com>
2017-05-14 02:03:56 +0300

    Create get GET request with parameters(course,group,weekday)

--- src/main/java/org/demo/demo/Controller/PairRestController.java
+++ src/main/java/org/demo/demo/Controller/PairRestController.java
@@ -1,9 +1,12 @@
 package org.demo.demo.Controller;
 
+import com.sun.org.apache.regexp.internal.RE;
 import org.demo.demo.entity.Pair;
 import org.demo.demo.repository.PairRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
@@ -11,10 +14,18 @@ import java.util.Collection;
 @RestController
 public class PairRestController {
 
-    @RequestMapping("/allPairs")
-    private Collection<Pair> pairs(){
+    @RequestMapping(value = "/allPairs", method = RequestMethod.GET)
+    private Collection<Pair> allPairs() {
         return this.pairRepository.findAll();
     }
+
+    @RequestMapping(value = "/pairs", method = RequestMethod.GET)
+    private Collection<Pair> pairsSuchCourseGroupWeekdayIndex(@RequestParam(value = "course", required = false) int course,
+                                                              @RequestParam(value = "group", required = false) int group,
+                                                              @RequestParam(value = "weekday", required = false) int weekday) {
+        return this.pairRepository.findPairsByStudentCourseAndStudentGroupAndWeekdayIndex(course, group, weekday);
+    }
+
 
     @Autowired
     PairRepository pairRepository;
